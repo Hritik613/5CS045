0660029c41d02604a09d304a8191643f7b42bd23
Hritik613 <h.pauljoy@wlv.ac.uk>
2025-11-27 13:11:29 +0000

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,30 @@
+<?php
+$mysqli = new mysqli("localhost", "2410923", "pp2410923", "db2410923");
+
+if (!isset($_GET['id'])) {
+    die("No book selected.");
+}
+
+$id = intval($_GET['id']);
+
+// Delete book image file if exists
+$result = $mysqli->query("SELECT image FROM `My books List` WHERE ID=$id");
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $imagePath = $row['image'];
+
+    if (!empty($imagePath) && file_exists($imagePath)) {
+        unlink($imagePath);
+    }
+}
+
+// Delete database row
+$delete = $mysqli->query("DELETE FROM `My books List` WHERE ID=$id");
+
+if ($delete) {
+    header("Location: index.php?msg=deleted");
+    exit();
+} else {
+    echo "Error deleting book: " . $mysqli->error;
+}
+?>
